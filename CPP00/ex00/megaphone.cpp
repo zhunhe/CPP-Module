7e151f772ee2070516299cec19a45e82233cbe1e
zhunhe <xuzhunhe@gmail.com>
2022-06-09 23:08:48 +0900

    refactor: Use string object

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,20 +6,22 @@
 /*   By: juhur <juhur@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 14:56:50 by juhur             #+#    #+#             */
-/*   Updated: 2022/06/09 14:38:17 by juhur            ###   ########.fr       */
+/*   Updated: 2022/06/09 21:52:13 by juhur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <cctype>
+#include <string>
 
 int main(int argc, char **argv) {
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else {
+		std::string str = "";
 		for (int i = 1; i < argc; i++)
-			for (int j = 0; argv[i][j] != '\0'; j++)
-				std::cout << static_cast<char>(toupper(argv[i][j]));
+			str += static_cast<std::string>(argv[i]);
+		for (size_t i = 0; i < str.size(); i++)
+			std::cout << static_cast<char>(toupper(str[i]));
 		std::cout << std::endl;
 	}
 	return 0;
